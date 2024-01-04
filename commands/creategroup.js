4a146a218b1eda33034c17187b00aea06bfc6779
CalebMNg <ngmcaleb@gmail.com>
2024-01-04 16:29:19 -0500

    change creategroup to embed

--- commands/creategroup.js
+++ commands/creategroup.js
@@ -4,6 +4,7 @@ const {
   SlashCommandBuilder,
   ActionRowBuilder,
   ComponentType,
+  EmbedBuilder,
 } = require("discord.js");
 
 const { openDb } = require("../handlers/databaseHandler.js");
@@ -11,7 +12,7 @@ const { openDb } = require("../handlers/databaseHandler.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("creategroup")
-    .setDescription("Creates a group and a message to add people to it")
+    .setDescription("Creates a group as an embeded message.")
     .addStringOption((option) =>
       option
         .setName("name")
@@ -24,20 +25,22 @@ module.exports = {
         .setDescription("The description of the group.")
     ),
 
-
   async execute(interaction) {
     const confirm = new ButtonBuilder()
-      .setCustomId("add")
-      .setLabel("Add")
+      .setCustomId("join")
+      .setLabel("Join")
       .setStyle(ButtonStyle.Primary);
 
     const row = new ActionRowBuilder().addComponents(confirm);
 
-    let description = interaction.options.getString('description');
-    if (!description) description = 'This is the default description.';
+    let description = interaction.options.getString("description");
+    if (!description) description = "This is the default description.";
+    const groupname = interaction.options.getString("name");
+
+    let embeded = new EmbedBuilder().setTitle(groupname).setDescription(description)
 
     const response = await interaction.reply({
-      content: description,
+      embeds: [embeded],
       components: [row],
     });
 
@@ -50,27 +53,31 @@ module.exports = {
     //add group to groups table
     const groupid = (await db.get("SELECT COUNT(groupid) AS count FROM groups"))
       .count;
-    const groupname = interaction.options.getString('name');
-    let sql =
-      'INSERT INTO groups (serverid, groupid, groupname) VALUES (?, ?, ?)';
+    let sql = "INSERT INTO groups (serverid, groupid, groupname) VALUES (?, ?, ?)";
     await db.run(sql, [interaction.guild.id, groupid, groupname]);
 
-
-    //add people
+    //join people
     collector.on("collect", async (i) => {
-      let sql = "SELECT 1 FROM groupusers WHERE serverid = ? AND groupid = ? AND userid = ?";
-      let exists = await db.get(sql, [interaction.guild.id, groupid, i.user.id]) != null;
-
+      let sql =
+        "SELECT 1 FROM groupusers WHERE serverid = ? AND groupid = ? AND userid = ?";
+      let exists =
+        (await db.get(sql, [interaction.guild.id, groupid, i.user.id])) != null;
       if (exists) {
-        let sql = "DELETE FROM groupusers WHERE serverid = ? AND groupid = ? AND userid = ?";
+        let sql =
+          "DELETE FROM groupusers WHERE serverid = ? AND groupid = ? AND userid = ?";
         await db.run(sql, [interaction.guild.id, groupid, i.user.id]);
-        await i.reply({content: `You have been removed from the group ${groupname}.`, ephemeral: true});
-      }
-
-      else {
-        let sql = "INSERT INTO groupusers (serverid, groupid, userid) VALUES (?, ?, ?)";
+        await i.reply({
+          content: `You have been removed from the group ${groupname}.`,
+          ephemeral: true,
+        });
+      } else {
+        let sql =
+          "INSERT INTO groupusers (serverid, groupid, userid) VALUES (?, ?, ?)";
         await db.run(sql, [interaction.guild.id, groupid, i.user.id]);
-        await i.reply({content: `You have been added from the group ${groupname}.`, ephemeral: true});
+        await i.reply({
+          content: `You have been added from the group ${groupname}.`,
+          ephemeral: true,
+        });
       }
     });
   },
